8048b51f0789eacd465d873e8d8eee7fb87264bb
pkamano <p.amano1225@protonmail.com>
2022-07-11 08:37:01 -0700

    completed assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -15,35 +15,136 @@
 
 // a) Create a test with an expect statement using the variable provided. HINT: Check out this resource: https://jestjs.io/docs/expect#expectarraycontainingarray
 
-const colors1 = ["purple", "blue", "green", "yellow", "pink"]
+//const colors1 = ["purple", "blue", "green", "yellow", "pink"]
 // Expected output example (can be a different order): ["yellow", "blue", "pink", "green"]
-const colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
+//const colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
 // Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
+describe('arrayContaining', () => {
+    const expected = ["blue", "green", "yellow", "pink"];
+    it('matches even if received contains less elements in a different order', () => {
+        expect(["purple", "blue", "green", "yellow", "pink"]).toEqual(expect.arrayContaining(expected));
+    });
+    it('does not match if received has "purple" in the array', () => {
+        expect(["purple"]).not.toEqual(expect.arrayContaining(expected))
+    });
+});
 
+//Output: ● arrayContaining › matches even if recieved contains less elements in a different order; expect(received).toEqual(expected) // deep equality
+describe('arrayContaining', () => {
+    const expected2 = ["indigo", "periwinkle", "ochre", "aquamarine", "saffron"];
+    it('matches even if received contains less elements in a different order', () => {
+        expect(["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]).toEqual(expect.arrayContaining(expected2))
+    });
+    it('does not match if received has "chartreuse" in the array', () => {
+        expect(["chartreuse"]).not.toEqual(expect.arrayContaining(expected2))
+    });
+});
 
 // b) Create the function that makes the test pass.
+//PsuedoCode
+//A function using the .shift() method, which would remove the value at the zeroth index ("purple" and "chartreuse") followed up by .sort() to put the values in the array in a random. The syllabus states that there are advanced sorting methods. After doing some research, I found using a randomIndex variable utilizing Math.floor(Math.random() * the current array) after .shift() might possibly work. Since I defined a result in the test, I hope "shuffling" the remain array values will still make the test pass. We've worked with randomizing numbers/ values in the past in class so using Math.floor(Math.random()) in some capacity  will probably be needed. 
+//Parameters:
+    //Input: //const colors1 = ["purple", "blue", "green", "yellow", "pink"], //const colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
+//Output: Expected output = ["pink", "blue", "yellow", "green"]; Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
 
+//I tried using the .shift() method but quickly realized that the original array would be affected so I was only getting "purple" as an output. Even when I .sort() the original array, I came back with "blue" as the output. So researching shuffling methods, random methods, I came across the Fisher Yates Method. I tested out the example function that W3 has. The issue I am having is trying to figure out how to get rid of the [0] color. I used .slice(0,1) in the console.log portion in hopes of getting back every color but purple together but I got back every color but purple, seperately. After using the .shift() again but declaring a new variable, newArr = colors1.shift() and put it above the Fisher Yates method, I was able to get purple to not be part of the output. 
+    //I had to go back and rework my test once I knew my function worked. I didn't comprehend what the example provided was stating. But after refactoring the values in the arrays, the test passed!
+
+const colors1 = ["purple", "blue", "green", "yellow", "pink"];
+    newArr = colors1.shift()
+    
+    for (let i = colors1.length -1; i > 0; i--) {
+      let j = Math.floor(Math.random() * i)
+      let k = colors1[i]
+      colors1[i] = colors1[j]
+      colors1[j] = k
+    }
+    console.log(colors1)
+
+const colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"];
+    newArr = colors2.shift()
+
+for (let i = colors2.length -1; i > 0; i--) {
+  let j = Math.floor(Math.random() * i)
+  let k = colors2[i]
+  colors2[i] = colors2[j]
+  colors2[j] = k
+}
+console.log(colors2)
 
 // --------------------2) Create a function that takes an array of numbers and returns an array of the minimum and maximum numbers in that order.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const nums1 = [3, 56, 90, -8, 0, 23, 6]
+//const nums1 = [3, 56, 90, -8, 0, 23, 6]
 // Expected output: [-8, 90]
-const nums2 = [109, 5, 9, 67, 8, 24]
-// Expected output: [5, 109]
+//const nums2 = [109, 5, 9, 67, 8, 24]
+//Expected output: [5, 109]
+describe('leastToGreatest', () => {
+    const expectedNums = [-8, 90];
+    it('takes an array of numbers and returns an array of the minimum and maximum numbers in that order', () => {
+        expect([3, 56, 90, -8, 0, 23, 6]).toEqual(expect.leastToGreatest(expectedNums))
+    });
+    it('does not match if received has other values besides [-8, 90]', () => {
+        expect([3, 56, 0, 23, 6]).not.toEqual(expect.leastToGreatest(expectedNums))
+    });
+});
 
+// TypeError: expect.leastToGreatest is not a function
 
 // b) Create the function that makes the test pass.
 
+//PseudoCode:
+//using the .sort() method to put the array from least to greatest value then .filter() to return those values in a seperate array. Or after sorting the values from least to greatest, have the function return the zero index and the last value possibly using .pop() and .shift()???
+
+//Parameters: Input:const nums1 = [3, 56, 90, -8, 0, 23, 6], Expected output: [-8, 90]
+//const nums2 = [109, 5, 9, 67, 8, 24], Expected output: [5, 109]
+
+// function leastToGreatest(array) {
+//             return nums1.sort(function(a, b){return a-b}).pop()
+//         }  
+//         console.log(leastToGreatest(nums1)) //90
+//     function leastToGreatest(array) {
+//             return nums1.sort(function(a, b){return a-b}).shift()
+//         }
+            
+//         console.log(leastToGreatest(nums1)) // -8
+//I could not figure out how to get these two results in the same function. I did try to make different variables and join the results that way but did not get very far. I was able to get the .sort() method down but it's just calling on the first and last values. I tried manipulating the return portion but was not sure if I had to include a for loop to access those indexes. Brining all the pieces together and writing it correctly is still a struggle for me. 
 
 // --------------------3) Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator to pass in a dynamic number of arguments.
 
 // a) Create a test with an expect statement using the variables provided.
 
-const testArray1 = [3, 7, 10, 5, 4, 3, 3]
-const testArray2 = [7, 8, 2, 3, 1, 5, 4]
+// const testArray1 = [3, 7, 10, 5, 4, 3, 3]
+// const testArray2 = [7, 8, 2, 3, 1, 5, 4]
 // Expected output: [3, 7, 10, 5, 4, 8, 2, 1]
 
+describe('twoToOneArr', () => {
+    const mergedArray = [3, 7, 10, 5, 4, 8, 2, 1];
+    it('takes in two arrays as arguments and returns one array with no duplicate values', () => {
+        expect([3, 7, 10, 5, 4, 3, 3], [7, 8, 2, 3, 1, 5, 4]).toEqual(expect.arrayContaining(mergedArray));
+    }) 
+    it('does not match if received has duplicate values in the array', () => {
+        expect([3, 3, 3, 3, 7, 7, 5, 5]).not.toEqual(expect.arrayContaining(mergedArray))
+    });
+});
+//Output: Expect takes at most one argument.
 
 // b) Create the function that makes the test pass.
+//PsuedoCode:
+//The arrays need to be put into one array using the spread operator. Even thought there will be duplicate values, it's easier to consolidate both arrays into one at the start. The next step would be to filter through the single merged array and have values that don't repeat be returned in the new array. 
+    //I wanted to use the spread operator because of 1. Trish's trick to remember it and 2. it seemed like it would be a better starting point and just simplify the function. I did have to research further on how to access values and have non-duplicate values being returned.  
+//After figuring out where to put .filter() I looked to the resources to remind me of the .indexOf() method. 
+//Parameters: const testArray1 = [3, 7, 10, 5, 4, 3, 3],const testArray2 = [7, 8, 2, 3, 1, 5, 4]
+//Output: [3, 7, 10, 5, 4, 8, 2, 1]
+
+const testArray1 = [3, 7, 10, 5, 4, 3, 3]
+const testArray2 = [7, 8, 2, 3, 1, 5, 4]
+const mergedTestArrs = [...testArray1, ...testArray2]
+//console.log(mergedTestArrs)   
+const noDups = mergedTestArrs.filter((value, index) => {
+    return mergedTestArrs.indexOf(value) === index;
+});
+console.log(noDups)
+
+//My function ended up working but it is my test that needs to be refactored. The tests were harder to write than the functions for this week's assessments. 😩 Also, for some reason, my tests would be running multiple times in the console.
--- interview-questions.md
+++ interview-questions.md
@@ -7,52 +7,50 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What are props in React?
 
-  Your answer:
+  Your answer: Props in React pass from parent to child classes and back upriver (child to parent) in between components. 
 
-  Researched answer:
-
+  Researched answer: Props pass information (data and methods) from one component to another in only one direction, from the parent to the child. Props takes certain information from state, and the props either display information or let the user interact with it. Props allows the application to be dynamic versus hardcoding information.
 
 
 2. What is a DOM event?
 
-  Your answer:
+  Your answer: A DOM event is when there's code being passed in state and props but nothing is rendered yet. 
 
-  Researched answer:
+  Researched answer: A DOM event is either HTML or React perfoming action based on user input. For example, onclick (HTML) & onClick (React) are DOM events. When the user clicks on a button, then a specified action will happen such as translating a word or words to pig latin.
 
 
 
 3. What is object-oriented programming? How is it different than functional programming?
 
-  Your answer:
+  Your answer: Object-oriented programming or OOP, is when every variable is treated as an object. Methods are able to run on objects.Objects are the intersection of data and behavior so treating every input as an object will return results more efficiently. 
 
-  Researched answer:
+  Researched answer: Object-oriented programming (OOP) was created to help the developer manage and maintain large programs without causing other parts of the software to experience negative side effects. Creating containers for sections of code allows the developers to have many small parts interact seperately versus the entire program being affected with one small change to a certain piece of code. 
 
 
 
 4. What is the difference between a Float and an Integer in Ruby?
 
-  Your answer:
+  Your answer: The difference between a Float and an Integer in Ruby is that an integer can have one decimal space and still be considered an integer. A float will have more than one decimal space. 
 
-  Researched answer:
-
+  Researched answer: The difference between a Float and an Integer in Ruby is that a Float needs to be passed in the equation in order to get a Float. An integer will not return any floats when used in an equation. For example, 5 / 3 will result in 1 when putting this equation in Ruby. If the equation is rewritten as 5.0/3.0 the result is 1.6666666666666667.
 
 
 5. Ruby has an implicit return. What does that mean?
 
-  Your answer:
+  Your answer: An implicit return is the last line in the code will be automatically returned without needing an invocation. 
 
-  Researched answer:
+  Researched answer: Implicit return is the value of the last line code will be automatically returned without needing an invocation.
 
 
 
 ## Looking Ahead: Terms for Next Week
 
-1. Instance Variable:
+1. Instance Variable: It is specific to the class it is in and can only be accessed by the set and get method.
 
-2. PostgreSQL:
+2. PostgreSQL: The language that is used to naviagte OOP databases.
 
-3. Ruby on Rails:
+3. Ruby on Rails: Creating an application utilizing ruby on rails framework that includes the bones for a database, web service and web pages. 
 
-4. ORM:
+4. ORM: Object-relational mapping - A technique used to have object oriented programming languages talk to one another and perform in a single database.
 
-5. Active Record:
+5. Active Record: The "in-between" that talks with the Rails application and the database.
